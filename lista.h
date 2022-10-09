8ab88389fd42df33b7114fd612febecdc3b75e57
Rafael Peres <65551999+rafa-speres@users.noreply.github.com>
2022-10-09 17:17:52 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -2,8 +2,34 @@
 //Rafael Scalon Peres Conti, nUSP: 11871181
 #ifndef LISTA_H
 #define LISTA_H
+//MENSAGENS DE RETURN
+
+typedef char t_chave;
+typedef unsigned int t_telefone
+
+typedef struct t_elemento {
+	t_chave nome[10];
+  t_telefone telefone;
+} t_elemento;
+
+typedef struct t_no *t_apontador;
+typedef struct t_no {
+	t_elemento elemento;
+	t_apontador proximo;
+} t_no;
+
+typedef struct {
+	t_apontador primeiro;
+} t_lista;
+
+int criar(t_lista *lista);
+t_elemento pesquisar(t_lista *lista, t_chave chave);
+int inserir(t_lista *lista, t_elemento elemento);
+int remover(t_lista *lista, t_chave chave);
+int alterar(t_lista *lista, t_telefone novo_telefone);
 /*
 .
 .
-.*/
+.
+*/
 #endif
