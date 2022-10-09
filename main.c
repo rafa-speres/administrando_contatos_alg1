8833e94a2d0371967375db6a0eba98d7380e29dc
Rafael Peres <65551999+rafa-speres@users.noreply.github.com>
2022-10-09 00:53:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,36 @@
+//Administrando contatinhos
+//Rafael Scalon Peres Conti, nUSP: 11871181
 
+#include<stdio.h>
+#include<stdlib.h>
+#include "lista.h"
+
+int main(){
+    //variavel para receber a operacao e um contador
+    char op_entrada = -1; //inicializado com operacao invalida
+    int i = 0;
+    while(i < 109 && op_entrada != 0){
+        switch (op_entrada){
+            case 'I': //inserir novo contato
+            
+            break;
+
+            case 'P': //pesquisar por um contato
+
+            break;
+
+            case 'R': //remover um contato ( busca por nome)
+
+            break;
+
+            case 'A': //alterar um telefone (busca por nome)
+
+            break;
+
+            case '0': //sair do loop e finalizar o programa
+            break;
+        }
+    }
+
+    return 0;
+}
