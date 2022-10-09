afd928763d35a8d2942fb9a5cbf62da3704cb746
Rafael Peres <65551999+rafa-speres@users.noreply.github.com>
2022-10-09 19:05:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,25 +3,47 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include "lista.h"
 
 int main(){
+    t_lista lista;
+    criar(&lista);
+    t_elemento elemento;
+    t_elemento elemento_aux;
+    t_apontador pointer;
     //variavel para receber a operacao e um contador
     char op_entrada = -1; //inicializado com operacao invalida
     int i = 0;
+    char nome[10];
+    unsigned int telefone;
     while(i < 109 && op_entrada != 0){
         scanf("%c", &op_entrada);
+        scanf("%s", elemento.nome);
+
         switch (op_entrada){
             case 'I': //inserir novo contato
-            
+                scanf("%u", &elemento.telefone);
+                elemento_aux = pesquisar(&lista, elemento.nome);
+                if(strcmp(elemento.nome, elemento_aux.nome)){
+                    printf("Contatinho ja inserido\n");
+                } else{
+                    inserir(&lista, elemento);
+                }
             break;
 
             case 'P': //pesquisar por um contato
+                elemento_aux = pesquisar(&lista, elemento.nome);
+                if(pesquisa_pos(&lista, elemento.nome) == NULL){
+                    printf("Operacao invalida: contatinho nao encontrado\n");
+                } else{
+                    printf("Contatinho encontrado: telefone %d\n", elemento_aux.telefone);
+                }
 
             break;
 
             case 'R': //remover um contato ( busca por nome)
-
+                
             break;
 
             case 'A': //alterar um telefone (busca por nome)
