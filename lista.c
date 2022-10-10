79e3f27c8241bded05da210378cee693e99e07bc
Rafael Peres <65551999+rafa-speres@users.noreply.github.com>
2022-10-10 17:45:19 -0300

    Add files via upload

--- lista.c
+++ lista.c
@@ -1,5 +1,9 @@
 //Administrando contatinhos
-//Rafael Scalon Peres Conti, nUSP: 11871181 - Giancarlo Malfate Caprino, nUSP: 12725025
+//Giancarlo Malfate Caprino, nUSP: 12725025
+//Henrique Gualberto Marques, nUSP: 13692380
+//Pedro Henrique Cruz da Silva, nUSP: 11833236
+//Rafael Scalon Peres Conti, nUSP: 11871181 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
