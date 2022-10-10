5e1a99453851599de31d45407fc83059a994c887
Rafael Peres <65551999+rafa-speres@users.noreply.github.com>
2022-10-10 03:03:32 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -9,13 +9,12 @@ void criar(t_lista *lista){
     lista->primeiro = NULL;
 }
 
-t_apontador pesquisa_ponteiro(t_lista *lista, t_chave *chave){
+static t_apontador pesquisa_pos(t_lista *lista, t_chave *chave){
 
 	t_apontador P = lista->primeiro;
 	if(P == NULL){
 		return NULL;
 	}
-	
 	while(P != NULL) {
 		if (!strcmp(P->elemento.nome, chave)){
 			return P;
@@ -27,20 +26,50 @@ t_apontador pesquisa_ponteiro(t_lista *lista, t_chave *chave){
 }
 
 t_elemento pesquisar(t_lista *lista, t_chave *chave){
-    t_apontador P = pesquisa_ponteiro(lista, chave);
+    t_apontador P = pesquisa_pos(lista, chave);
 	return P->elemento;
 }
 
 int inserir(t_lista *lista, t_elemento elemento){
-	t_apontador novo = (t_apontador) malloc(sizeof(t_no));
-	if (novo == NULL)
-		return 0; //se memoria cheia
+	if(pesquisa_pos(lista, elemento.nome) == NULL){ //se o nao houver um elemento com a mesma chave
+		t_apontador novo = (t_apontador) malloc(sizeof(t_no));
+		if (novo == NULL){
+			return MEMORIA_CHEIA;
+		}
 
-	novo->elemento = elemento;
-	novo->proximo = lista->primeiro;
-	lista->primeiro = novo;
+		novo->elemento = elemento;
+		novo->proximo = lista->primeiro;
+		lista->primeiro = novo;
 
-	return 1;
+		return SUCESSO;
+	}else{
+		return ELEMENTO_REPETIDO;
+	}
+
 }
-int remover(t_lista *lista, t_chave *chave);
-int alterar(t_lista *lista, t_telefone novo_telefone);
+int remover(t_lista *lista, t_chave *chave){
+	t_apontador P = lista->primeiro;
+	if(P==NULL){
+		return ELEMENTO_NAO_ENCONTRADO;
+	}
+	
+	if(strcmp(P->elemento.nome, chave) == 0){ //elemento a ser removido e' o primeiro da lista
+		lista->primeiro = lista->primeiro->proximo;
+		free(P);
+		return SUCESSO;
+	}
+
+	while(P->proximo != NULL){
+		if(strcmp(P->elemento.nome, chave) == 0){
+			t_apontador auxiliar = P->proximo;
+			P->proximo = P->proximo->proximo;
+			free(auxiliar);
+			return SUCESSO;
+		}
+		P = P->proximo;
+	}
+
+	return ELEMENTO_NAO_ENCONTRADO;
+}
+
+int alterar(t_lista *lista, t_chave *chave, t_telefone novo_telefone);
